521f5d717dd2164cc2c544287c8ef8ee1d902c09
RoyalEmpoleon <jkoutsourakis.682@naot.edu.au>
2017-01-16 18:14:05 +1100

    My commit message

---
+++ app/src/main/java/com/newyorkinmelbourne/nu_vet/MainActivity.java
@@ -0,0 +1,43 @@
+package com.newyorkinmelbourne.nu_vet;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends ActionBarActivity {
+    private WebView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view = (WebView) this.findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient(new MyBrowser());
+        view.loadUrl("http://www.nu-vet.com"); //nu-vet site
+        view.setWebChromeClient(new WebChromeClient()); // adding js alert support
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && view.canGoBack()) {
+            view.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
